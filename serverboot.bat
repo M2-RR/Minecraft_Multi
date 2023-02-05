c7cfd6486887cc2144774126647d2408ce466761
Mikky.T <98258694+M2-RR@users.noreply.github.com>
2023-02-05 13:44:13 +0900

    Update serverboot.bat

--- serverboot.bat
+++ serverboot.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Minecraft Launcher\runtime\java-runtime-beta\windows-x64\java-runtime-beta\bin\java" -jar server.jar nogui
+"C:\Program Files (x86)\Minecraft Launcher\runtime\java-runtime-beta\windows-x64\java-runtime-beta\bin\java" -jar server.jar
